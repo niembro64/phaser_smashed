2dfc710a00e9c3fc7960e7257fb4b39ab84eaa33
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-07-25 23:28:10 -0400

    4 chars

--- src/PhaserGame.ts
+++ src/PhaserGame.ts
@@ -1,22 +1,22 @@
-import Game from './scenes/Game'
+import Game from "./scenes/Game";
 
-const config: Phaser.Types.Core.GameConfig =  {
-  scale: {
-      width: 800,
-      height: 400,
-  },
-  pixelArt: false,
-  type: Phaser.AUTO,
-  parent: "game",
-  backgroundColor: "#0072bc",
-  physics: {
-      default: "arcade",
-      arcade: {
-          gravity: { y: 300 },
-          debug: false,
-      },
-  },
-  scene: Game,
+const config: Phaser.Types.Core.GameConfig = {
+    scale: {
+        width: 800,
+        height: 400,
+    },
+    pixelArt: false,
+    type: Phaser.AUTO,
+    parent: "game",
+    backgroundColor: "#0072bc",
+    physics: {
+        default: "arcade",
+        arcade: {
+            //   gravity: { y: 300 },
+            debug: false,
+        },
+    },
+    scene: Game,
 };
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,22 +1,103 @@
 import { SignPrivateKeyInput } from "crypto";
 import "phaser";
 
+export interface player {
+    char: char;
+}
+
+export interface char {
+    sprite: null | Phaser.GameObjects.Sprite;
+    position: { x: number; y: number };
+    velocity: { x: number; y: number };
+    acceleration: { x: number; y: number };
+    damage: number;
+}
+
 export default class Game extends Phaser.Scene {
-    char0: any;
+    screen = { height: 800, width: 400 };
+    defaults = { position: { playerY: 100 } };
+    gravity: number = 0.2;
+    players: player[] = [
+        {
+            char: {
+                sprite: null,
+                position: { x: 0, y: 0 },
+                velocity: { x: 0, y: 0 },
+                acceleration: { x: 0, y: 0.2 },
+                damage: 0,
+            },
+        },
+        {
+            char: {
+                sprite: null,
+                position: { x: 0, y: 0 },
+                velocity: { x: 0, y: 0 },
+                acceleration: { x: 0, y: 0.4 },
+                damage: 0,
+            },
+        },
+        {
+            char: {
+                sprite: null,
+                position: { x: 0, y: 0 },
+                velocity: { x: 0, y: 0 },
+                acceleration: { x: 0, y: 0.5 },
+                damage: 0,
+            },
+        },
+        {
+            char: {
+                sprite: null,
+                position: { x: 0, y: 0 },
+                velocity: { x: 0, y: 0 },
+                acceleration: { x: 0, y: 0.9 },
+                damage: 0,
+            },
+        },
+    ];
     constructor() {
         super("game");
     }
 
     preload() {
-        this.load.image("char0", "../images/star.png");
+        this.players.forEach((p, i) => {
+            this.load.image("c" + i.toString(), "character_" + i + ".png");
+        });
     }
 
     create() {
-        this.char0 = this.physics.add.sprite(100, 100, "char0");
+        this.players.forEach((p, i) => {
+            p.char.sprite = this.physics.add.sprite(
+                200 * i + 100,
+                this.defaults.position.playerY,
+                "c" + i.toString()
+            );
+            console.log(p.char.sprite);
+        });
     }
     update() {
-        if (this.char0.y > 300) {
-            this.char0.y = 100;
-        }
+        updateposition(this);
+        updateAllMovingParts(this);
     }
+}
+
+export function updateposition(g: Game): void {
+    g.players.forEach((p, i) => {
+        if (!p.char.sprite) {
+            return;
+        }
+        p.char.velocity.y += g.gravity + p.char.acceleration.y;
+        p.char.sprite.y += p.char.velocity.y;
+    });
+}
+
+export function updateAllMovingParts(g: Game): void {
+    g.players.forEach((p, i) => {
+        if (!p.char.sprite) {
+            return;
+        }
+        if (p.char.sprite.y > g.screen.height) {
+            p.char.sprite.y = 0;
+        }
+    });
 }
