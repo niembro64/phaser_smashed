56ebe3d3a748f868271ff2b004987d07a9f57b08
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-07-26 01:26:23 -0400

    needs some work

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,62 +2,108 @@ import "phaser";
 
 export interface player {
     char: char;
+    KEYBOARD: KEYBOARD;
+    keyboard: typeof Phaser.Input.Keyboard | any;
+}
+export interface KEYBOARD {
+    up: typeof Phaser.Input.Keyboard.Key | any;
+    down: typeof Phaser.Input.Keyboard.Key | any;
+    left: typeof Phaser.Input.Keyboard.Key | any;
+    right: typeof Phaser.Input.Keyboard.Key | any;
+    fast: typeof Phaser.Input.Keyboard.Key | any;
+    jump: typeof Phaser.Input.Keyboard.Key | any;
 }
 
 export interface char {
     sprite: any | Phaser.GameObjects.Sprite;
-    position: { x: number; y: number };
-    velocity: { x: number; y: number };
-    acceleration: { x: number; y: number };
+    pos: { x: number; y: number };
+    vel: { x: number; y: number };
+    acc: { x: number; y: number };
     damage: number;
 }
 
 export default class Game extends Phaser.Scene {
     screen = { height: 800, width: 400 };
-    defaults = { position: { playerY: 100 } };
-    gravity: number = 0.2;
+    defaults = { pos: { playerY: 100 } };
+    gravity: number = 1;
     players: player[] = [
         {
             char: {
                 sprite: 0,
-                position: { x: 0, y: 0 },
-                velocity: { x: 0, y: 0 },
-                acceleration: { x: 0, y: 0.2 },
+                pos: { x: 0, y: 0 },
+                vel: { x: 0, y: 0 },
+                acc: { x: 0, y: 0.2 },
                 damage: 0,
             },
+            KEYBOARD: {
+                up: Phaser.Input.Keyboard.KeyCodes.W,
+                down: Phaser.Input.Keyboard.KeyCodes.S,
+                left: Phaser.Input.Keyboard.KeyCodes.A,
+                right: Phaser.Input.Keyboard.KeyCodes.D,
+                fast: Phaser.Input.Keyboard.KeyCodes.Z,
+                jump: Phaser.Input.Keyboard.KeyCodes.X,
+            },
+            keyboard: 0,
         },
         {
             char: {
                 sprite: 0,
-                position: { x: 0, y: 0 },
-                velocity: { x: 0, y: 0 },
-                acceleration: { x: 0, y: 0.4 },
+                pos: { x: 0, y: 0 },
+                vel: { x: 0, y: 0 },
+                acc: { x: 0, y: 0.4 },
                 damage: 0,
             },
+            KEYBOARD: {
+                up: Phaser.Input.Keyboard.KeyCodes.T,
+                down: Phaser.Input.Keyboard.KeyCodes.G,
+                left: Phaser.Input.Keyboard.KeyCodes.F,
+                right: Phaser.Input.Keyboard.KeyCodes.H,
+                fast: Phaser.Input.Keyboard.KeyCodes.V,
+                jump: Phaser.Input.Keyboard.KeyCodes.B,
+            },
+            keyboard: 0,
         },
         {
             char: {
                 sprite: 0,
-                position: { x: 0, y: 0 },
-                velocity: { x: 0, y: 0 },
-                acceleration: { x: 0, y: 0.5 },
+                pos: { x: 0, y: 0 },
+                vel: { x: 0, y: 0 },
+                acc: { x: 0, y: 0.5 },
                 damage: 0,
             },
+            KEYBOARD: {
+                up: Phaser.Input.Keyboard.KeyCodes.I,
+                down: Phaser.Input.Keyboard.KeyCodes.K,
+                left: Phaser.Input.Keyboard.KeyCodes.J,
+                right: Phaser.Input.Keyboard.KeyCodes.L,
+                fast: Phaser.Input.Keyboard.KeyCodes.O,
+                jump: Phaser.Input.Keyboard.KeyCodes.P,
+            },
+            keyboard: 0,
         },
         {
             char: {
                 sprite: 0,
-                position: { x: 0, y: 0 },
-                velocity: { x: 0, y: 0 },
-                acceleration: { x: 0, y: 0.9 },
+                pos: { x: 0, y: 0 },
+                vel: { x: 0, y: 0 },
+                acc: { x: 0, y: 0.9 },
                 damage: 0,
             },
+            KEYBOARD: {
+                up: Phaser.Input.Keyboard.KeyCodes.UP,
+                down: Phaser.Input.Keyboard.KeyCodes.DOWN,
+                left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+                right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+                fast: Phaser.Input.Keyboard.KeyCodes.END,
+                jump: Phaser.Input.Keyboard.KeyCodes.PAGE_DOWN,
+            },
+            keyboard: 0,
         },
     ];
     constructor() {
         super("game");
     }
-    platform: any | Phaser.GameObjects.Sprite;
+    platforms: any | Phaser.GameObjects.Sprite;
     preload() {
         this.load.image("platform", "platform.png");
         for (let i = 0; i < 4; i++) {
@@ -66,49 +112,66 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
-        this.platform = this.physics.add.staticGroup();
-        this.platform.create(300, 300, "platform").setScale(1, 1);
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(300, 300, "platform").setScale(1, 1);
         this.players.forEach((p, i) => {
             p.char.sprite = this.physics.add.sprite(
                 200 * i + 100,
-                this.defaults.position.playerY,
+                this.defaults.pos.playerY,
                 "c" + i.toString()
             );
-            p.char.sprite.setScale(1, 1);
-            console.log(p.char.sprite);
+
+            p.char.pos.x = 200 * i + 100;
+            p.char.sprite.setScale(1);
+            p.char.sprite.setCollideWorldBounds(true);
+            this.physics.add.collider(p.char.sprite, this.platforms);
+            p.keyboard = this.input.keyboard.addKeys(p.KEYBOARD);
         });
     }
     update() {
-        updateposition(this);
-        updateAllMovingParts(this);
+        updateKeyboard(this);
+        updateMovingData(this);
+        updateMovingSprite(this);
         printStatus(this);
     }
 }
-
-export function updateposition(g: Game): void {
+export function updateKeyboard(g: Game): void {
     g.players.forEach((p, i) => {
-        if (!p.char.sprite) {
-            return;
+        if (p.keyboard.right.isDown) {
+            p.char.vel.x = -30;
         }
-        p.char.velocity.y += g.gravity + p.char.acceleration.y;
-        p.char.sprite.y += p.char.velocity.y;
+        if (p.keyboard.left.isDaown) {
+            p.char.vel.x = 30;
+        }
     });
 }
-
-export function updateAllMovingParts(g: Game): void {
+export function updateMovingSprite(g: Game): void {
     g.players.forEach((p, i) => {
-        if (!p.char.sprite) {
-            return;
+        p.char.sprite.setVelocityX(p.char.vel.x);
+        p.char.sprite.setVelocityY(p.char.vel.y);
+    });
+}
+export function updateMovingData(g: Game): void {
+    g.players.forEach((p, i) => {
+        if (
+            p.char.sprite.body.touching.down ||
+            p.char.sprite.body.touching.up
+        ) {
+            p.char.vel.y = 0;
+            p.char.acc.y = 0;
         }
-        if (p.char.sprite.y > g.screen.height) {
-            p.char.sprite.y = 0;
+        if (
+            p.char.sprite.body.touching.left ||
+            p.char.sprite.body.touching.right
+        ) {
+            p.char.vel.x = 0;
+            p.char.acc.x = 0;
         }
+        p.char.vel.x += p.char.acc.x;
+        p.char.vel.y += g.gravity + p.char.acc.y;
     });
 }
 
 export function printStatus(g: Game): void {
-    if (!g.players[0].char.sprite) {
-        return;
-    }
     console.log("0", Math.round(g.players[0].char.sprite.y));
 }
