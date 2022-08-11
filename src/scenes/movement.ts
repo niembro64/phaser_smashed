4c80c0cd695af085f6377f7b3643131774be5a95
Eric Niemeyer <niemeyer.eric@gmail.com>
2022-08-11 16:22:25 -0400

    small jump edits

--- src/scenes/movement.ts
+++ src/scenes/movement.ts
@@ -1,160 +1,161 @@
-import Game from "./Game";
-import { Player } from "./interfaces";
+import Game from './Game';
+import { Player } from './interfaces';
 
 export function updateKeepOnScreen(player: Player, game: Game): void {
-  if (player.char.sprite.y < 0) {
-    player.char.sprite.y = game.SCREEN_DIMENSIONS.HEIGHT;
-  }
-  if (player.char.sprite.y > game.SCREEN_DIMENSIONS.HEIGHT) {
-    player.char.sprite.y = 0;
-  }
-  if (player.char.sprite.x < 0) {
-    player.char.sprite.x = game.SCREEN_DIMENSIONS.WIDTH;
-  }
-  if (player.char.sprite.x > game.SCREEN_DIMENSIONS.WIDTH) {
-    player.char.sprite.x = 0;
-  }
+    if (player.char.sprite.y < 0) {
+        player.char.sprite.y = game.SCREEN_DIMENSIONS.HEIGHT;
+    }
+    if (player.char.sprite.y > game.SCREEN_DIMENSIONS.HEIGHT) {
+        player.char.sprite.y = 0;
+    }
+    if (player.char.sprite.x < 0) {
+        player.char.sprite.x = game.SCREEN_DIMENSIONS.WIDTH;
+    }
+    if (player.char.sprite.x > game.SCREEN_DIMENSIONS.WIDTH) {
+        player.char.sprite.x = 0;
+    }
 }
 export function updateLastDirectionTouched(player: Player): void {
-  if (player.char.sprite.body.touching.up) {
-    player.char.lastDirectionTouched = "up";
-  }
-  if (player.char.sprite.body.touching.down) {
-    player.char.lastDirectionTouched = "down";
-  }
-  if (player.char.sprite.body.touching.left) {
-    player.char.lastDirectionTouched = "left";
-  }
-  if (player.char.sprite.body.touching.right) {
-    player.char.lastDirectionTouched = "right";
-  }
+    if (player.char.sprite.body.touching.up) {
+        player.char.lastDirectionTouched = 'up';
+    }
+    if (player.char.sprite.body.touching.down) {
+        player.char.lastDirectionTouched = 'down';
+    }
+    if (player.char.sprite.body.touching.left) {
+        player.char.lastDirectionTouched = 'left';
+    }
+    if (player.char.sprite.body.touching.right) {
+        player.char.lastDirectionTouched = 'right';
+    }
 }
 
 export function updateWallTouchArray(game: Game): void {
-  game.players.forEach((player) => {
-    if (
-      player.char.sprite.body.touching.left ||
-      player.char.sprite.body.touching.right
-    ) {
-      player.char.wallTouchArray[game.allPlayersWallTouchIterator] = true;
-    } else {
-      player.char.wallTouchArray[game.allPlayersWallTouchIterator] = false;
-    }
-  });
-  game.allPlayersWallTouchIterator =
-    (game.allPlayersWallTouchIterator + 1) %
-    game.players[0].char.wallTouchArray.length;
+    game.players.forEach((player) => {
+        if (
+            player.char.sprite.body.touching.left ||
+            player.char.sprite.body.touching.right
+        ) {
+            player.char.wallTouchArray[game.allPlayersWallTouchIterator] = true;
+        } else {
+            player.char.wallTouchArray[game.allPlayersWallTouchIterator] =
+                false;
+        }
+    });
+    game.allPlayersWallTouchIterator =
+        (game.allPlayersWallTouchIterator + 1) %
+        game.players[0].char.wallTouchArray.length;
 }
 
 export function hasPlayerTouchedWallRecently(player: Player): boolean {
-  return !player.char.wallTouchArray.every((b) => b === false);
+    return !player.char.wallTouchArray.every((b) => b === false);
 }
 
 export function jump(player: Player, game: Game): void {
-  if (
-    player.char.sprite.body.touching.down ||
-    player.char.sprite.body.touching.left ||
-    player.char.sprite.body.touching.right
-  ) {
-    player.char.jumpIndex = 0;
-  }
-  if (player.pad.A && !player.padPrev.A) {
-    console.log(
-      "JUMP",
-      "player.index:",
-      player.index,
-      "jumpIndex",
-      player.char.jumpIndex,
-      "jumps[i]",
-      player.char.jumps[player.char.jumpIndex]
-    );
     if (
-      !(
         player.char.sprite.body.touching.down ||
         player.char.sprite.body.touching.left ||
         player.char.sprite.body.touching.right
-      ) &&
-      player.char.jumpIndex < 1
     ) {
-      player.char.jumpIndex = 1;
+        player.char.jumpIndex = 0;
     }
-    player.char.sprite.body.setVelocityY(
-      player.char.sprite.body.velocity.y *
-        (1 - player.char.jumps[player.char.jumpIndex]) -
-        game.DEFAULT_JUMP *
-          player.char.jumpPower *
-          player.char.jumps[player.char.jumpIndex]
-    );
-    player.char.jumpIndex +=
-      player.char.jumpIndex == player.char.jumps.length - 1 ? 0 : 1;
+    if (player.pad.A && !player.padPrev.A) {
+        console.log(
+            'JUMP',
+            'player.index:',
+            player.index,
+            'jumpIndex',
+            player.char.jumpIndex,
+            'jumps[i]',
+            player.char.jumps[player.char.jumpIndex]
+        );
+        if (
+            !(
+                player.char.sprite.body.touching.down ||
+                player.char.sprite.body.touching.left ||
+                player.char.sprite.body.touching.right
+            ) &&
+            player.char.jumpIndex < 1
+        ) {
+            player.char.jumpIndex = 1;
+        }
+        player.char.sprite.body.setVelocityY(
+            player.char.sprite.body.velocity.y *
+                (1 - player.char.jumps[player.char.jumpIndex]) -
+                game.DEFAULT_JUMP *
+                    player.char.jumpPower *
+                    player.char.jumps[player.char.jumpIndex]
+        );
+        player.char.jumpIndex +=
+            player.char.jumpIndex == player.char.jumps.length - 1 ? 0 : 1;
 
-    // horizontal stuff
-    if (
-      player.char.lastDirectionTouched === "left" &&
-      hasPlayerTouchedWallRecently(player)
-    ) {
-      player.char.sprite.body.setVelocityX(
-        game.DEFAULT_SPEED_X * player.char.speed * 30
-      );
-      return;
+        // horizontal stuff
+        if (
+            player.char.lastDirectionTouched === 'left' &&
+            hasPlayerTouchedWallRecently(player)
+        ) {
+            player.char.sprite.body.setVelocityX(
+                game.DEFAULT_SPEED_X * player.char.speed * 30
+            );
+            return;
+        }
+        if (player.pad.left && player.char.sprite.body.velocity.x > 0) {
+            player.char.sprite.body.setVelocityX(-game.DEFAULT_SPEED_X * 10);
+            return;
+        }
+        if (
+            player.char.lastDirectionTouched === 'right' &&
+            hasPlayerTouchedWallRecently(player)
+        ) {
+            player.char.sprite.body.setVelocityX(
+                -game.DEFAULT_SPEED_X * player.char.speed * 30
+            );
+            return;
+        }
+        if (player.pad.right && player.char.sprite.body.velocity.x < 0) {
+            player.char.sprite.body.setVelocityX(game.DEFAULT_SPEED_X * 10);
+            return;
+        }
     }
-    if (player.pad.left && player.char.sprite.body.velocity.x > 0) {
-      player.char.sprite.body.setVelocityX(-game.DEFAULT_SPEED_X * 10);
-      return;
-    }
-    if (
-      player.char.lastDirectionTouched === "right" &&
-      hasPlayerTouchedWallRecently(player)
-    ) {
-      player.char.sprite.body.setVelocityX(
-        -game.DEFAULT_SPEED_X * player.char.speed * 30
-      );
-      return;
-    }
-    if (player.pad.right && player.char.sprite.body.velocity.x < 0) {
-      player.char.sprite.body.setVelocityX(game.DEFAULT_SPEED_X * 10);
-      return;
-    }
-  }
 }
 
 export function frictionWallY(player: Player, game: Game): void {
-  if (
-    (player.pad.left || player.pad.right) &&
-    (player.char.sprite.body.touching.left ||
-      player.char.sprite.body.touching.right)
-  ) {
-    player.char.sprite.body.setVelocityY(0);
-  }
+    if (
+        (player.pad.left || player.pad.right) &&
+        (player.char.sprite.body.touching.left ||
+            player.char.sprite.body.touching.right)
+    ) {
+        player.char.sprite.body.setVelocityY(0);
+    }
 }
 
 export function frictionAirY(player: Player, game: Game): void {
-  if (!player.char.sprite.body.touching.down) {
-    player.char.sprite.body.setVelocityY(
-      player.char.sprite.body.velocity.y *
-        Math.pow(player.char.friction_air, 1 / player.char.friction_air)
-    );
-  }
+    if (!player.char.sprite.body.touching.down) {
+        player.char.sprite.body.setVelocityY(
+            player.char.sprite.body.velocity.y *
+                Math.pow(player.char.friction_air, 1 / player.char.friction_air)
+        );
+    }
 }
 export function frictionAirX(player: Player, game: Game): void {
-  if (!player.char.sprite.body.touching.down) {
-    player.char.sprite.body.setVelocityX(
-      player.char.sprite.body.velocity.x *
-        Math.pow(player.char.friction_air, 1.2)
-    );
-  }
+    if (!player.char.sprite.body.touching.down) {
+        player.char.sprite.body.setVelocityX(
+            player.char.sprite.body.velocity.x *
+                Math.pow(player.char.friction_air, 1.2)
+        );
+    }
 }
 export function frictionGroundX(player: Player, game: Game): void {
-  if (
-    player.char.sprite.body.touching.down &&
-    !player.pad.left &&
-    !player.pad.right
-  ) {
-    player.char.sprite.body.setVelocityX(
-      player.char.sprite.body.velocity.x *
-        Math.pow(player.char.friction_ground, 4)
-    );
-  }
+    if (
+        player.char.sprite.body.touching.down &&
+        !player.pad.left &&
+        !player.pad.right
+    ) {
+        player.char.sprite.body.setVelocityX(
+            player.char.sprite.body.velocity.x *
+                Math.pow(player.char.friction_ground, 4)
+        );
+    }
 }
 
 // export function addKeyboard(player: Player, game: Game): void {
