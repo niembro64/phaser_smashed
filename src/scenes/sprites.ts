965c9ffa75306309bd5606c457d015e741af37a8
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-08-11 04:26:24 -0400

    flipX

--- src/scenes/sprites.ts
+++ src/scenes/sprites.ts
@@ -6,9 +6,11 @@ export function updateSprites(game: Game): void {
     if (!hasPlayerTouchedWallRecently(player)) {
       if (player.char.sprite.body.velocity.x == 0) {
       } else if (player.char.sprite.body.velocity.x > 0) {
-        player.char.sprite.scaleX = 1;
+        player.char.sprite.flipX = false;
+        // player.char.sprite.scaleX = 1;
       } else {
-        player.char.sprite.scaleX = -1;
+        player.char.sprite.flipX = true;
+        // player.char.sprite.scaleX = -1;
       }
     }
   });
