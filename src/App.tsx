22c75fa919bcb599ef6e8410f27d606b6761899a
Eric Niemeyer <niemeyer.eric@gmail.com>
2022-09-14 00:18:17 -0400

    projects button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [debug, getDebug] = useState(true);
-  const [numControllers, getNumController] = useState(0);
+  // const [debug, getDebug] = useState(true);
+  // const [numControllers, getNumController] = useState(0);
 
   return (
     <>
+      <a id="linkTag" className="btn btn-outline-light" href="http://niembro64.com/">
+        <span>NIEMBRO64 PROJECTS</span>
+      </a>
       {/* <div id="top-bar">
         <h1 id="title">SMASHED</h1>
         <h1 id="title">{{ debug } ? "DEBUGGING" : ""}</h1>
