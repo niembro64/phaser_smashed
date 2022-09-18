68cc01743abf517d3d17760ec3c7ad281d145b47
Eric Niemeyer <niemeyer.eric@gmail.com>
2022-09-18 18:03:04 -0400

    loops skipped 0

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -44,7 +44,7 @@ export default class Game extends Phaser.Scene {
     useReadySound: false,
     invertHealth: false,
     seeSplashDataAlways: false,
-    numUpdateLoopsToSkip:  100,
+    numUpdateLoopsToSkip:  0,
   };
 
   DEFAULT_PLAYER_HITBACK: any = { x: 0.03, y: -0.03 };
