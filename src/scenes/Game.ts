90c8e43f4ced00be5d0486626c461001af862ecf
Eric Niemeyer <niemeyer.eric@gmail.com>
2022-10-07 19:03:05 -0400

    zoom messin

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -43,8 +43,8 @@ export default class Game extends Phaser.Scene {
   FLAG: any | Phaser.GameObjects.Sprite;
   ZOOM_MULTIPLIER_X = 0.95;
   ZOOM_MULTIPLIER_Y = 0.7;
-  ZOOM_RATIO_SLOW = 0.995;
-  ZOOM_RATIO_FAST = 0.5;
+  ZOOM_RATIO_SLOW = 0.9995;
+  ZOOM_RATIO_FAST = 0.95;
   BORDER_PADDING_X: number = 200;
   BORDER_PADDING_Y: number = 100;
   CAMERA_OFFSET_Y: number = -50;
