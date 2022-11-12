8c7373745b6e575c6c3ff3cae4bde46d3993da95
Eric Niemeyer <niemeyer.eric@gmail.com>
2022-11-12 17:04:36 -0500

    emitterPlayer off when pause

--- src/scenes/helpers/physics.ts
+++ src/scenes/helpers/physics.ts
@@ -1,10 +1,14 @@
-import Game from "../Game";
+import Game from '../Game';
 
 export function setPhysicsPause(game: Game): void {
   game.physics.pause();
+  game.players.forEach((player, playerIndex) => {
+    player.emitterPlayer.active = false;
+  });
 }
 export function setPhysicsResume(game: Game): void {
   game.physics.resume();
+  game.players.forEach((player, playerIndex) => {
+    player.emitterPlayer.active = true;
+  });
 }
-
-
