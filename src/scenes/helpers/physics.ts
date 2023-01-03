791ce5a2b32263089b6b608c4c3b4c5cc0b7841e
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-01-03 01:32:02 -0500

    pausing dark emitters

--- src/scenes/helpers/physics.ts
+++ src/scenes/helpers/physics.ts
@@ -5,13 +5,17 @@ export function setPhysicsPause(game: Game): void {
   game.physics.pause();
   game.players.forEach((player, playerIndex) => {
     player.emitterPlayer.active = false;
+    player.emitterDark.active = false;
   });
+  game.chomp.emitterDark.active = false;
   setAnimationsOff(game);
 }
 export function setPhysicsResume(game: Game): void {
   game.physics.resume();
   game.players.forEach((player, playerIndex) => {
     player.emitterPlayer.active = true;
+    player.emitterDark.active = true;
   });
+  game.chomp.emitterDark.active = true;
   setAnimationsOn(game);
 }
