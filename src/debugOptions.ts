9ec4c637846fc91d8a63e9490c8319a28d00c448
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-01-10 23:02:45 -0500

    new file for debug info

---
+++ src/debugOptions.ts
@@ -0,0 +1,73 @@
+import { Debug } from './scenes/interfaces';
+
+export const debugInit: Debug = {
+  DevMode: true,
+  DevZoom: 8,
+  Level: 6, //0123456
+  ModeInfinity: true,
+  InfinityShots: 7,
+  TimeMinutes: 7, //01234567
+  MusicActive: true,
+  MusicTrack: 2, // 012
+  ReadySoundActive: true,
+  Camera: true,
+  CamerasVisible: false,
+  CollidersPvP: false,
+  CollidersPvAP: false,
+  CollidersPvAE: false,
+  CollidersAEvAE: true,
+  CollidersAEvAP: true,
+  FrictionAirActive: true,
+  AEWrapScreen: false,
+  WallJumpsActive: true,
+  DefaultDamage: false,
+  DefaultHitback: false,
+  PlayerIdVisible: true,
+  CharacterTinted: false,
+  HealthInverted: false,
+  MatricesAlways: false,
+  ConsoleLogButtons: false,
+  ConsoleLogConnected: false,
+  UpdateLoopsNumSkip: 2, // 012
+  LoadTimeExtra: true,
+  AllowCharsExtended: true,
+  AllowCharsChez: false,
+  SlowMoExplosions: false,
+  TypedLoadingText: false,
+};
+
+export const debugMax: Debug = {
+  DevMode: true,
+  DevZoom: 10,
+  Level: 6, //0123456
+  ModeInfinity: true,
+  InfinityShots: 15,
+  TimeMinutes: 15, //01234567
+  MusicActive: true,
+  MusicTrack: 2, // 012
+  ReadySoundActive: true,
+  Camera: true,
+  CamerasVisible: false,
+  CollidersPvP: false,
+  CollidersPvAP: false,
+  CollidersPvAE: false,
+  CollidersAEvAE: true,
+  CollidersAEvAP: true,
+  FrictionAirActive: true,
+  AEWrapScreen: false,
+  WallJumpsActive: true,
+  DefaultDamage: false,
+  DefaultHitback: false,
+  PlayerIdVisible: true,
+  CharacterTinted: false,
+  HealthInverted: false,
+  MatricesAlways: false,
+  ConsoleLogButtons: false,
+  ConsoleLogConnected: false,
+  UpdateLoopsNumSkip: 2, // 012
+  LoadTimeExtra: true,
+  AllowCharsExtended: true,
+  AllowCharsChez: false,
+  SlowMoExplosions: true,
+  TypedLoadingText: false,
+};
